2437f2aed5bb59d17b33f75d36f6671a99069aa5
sewwanis <sewwanis@gmail.com>
2018-07-02 11:36:15 -0700

    second commit

--- project.c
+++ project.c
@@ -1,23 +1,67 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+/*struct node{
+	int length;
+	struct Node *next;
+};
+*/
+
+
+//this function is to print the matrix
+void print_matrix(char matrix,int n){
+
+}
 int main(){
 
-	char s[100];
+	char word[100];
 	int k = 1;
+	char matrix[i][j];
+	int i,j;
 
 	while(1){
-		gets(s);
- 		printf("\nLength of input : %d\n", strlen(s));
- 		if(s[0]== NULL){
+		//gets(word);
+		word = getchar();
+ 		printf("\nLength of input : %d\n", strlen(word));
+ 		/*if(word[0] == NULL){
 			printf("Null test 1");
 			break;
-		}
-		if(strlen(s) == 0){
+		}*/
+		if(strlen(word) == 0){
 			printf("Null test 2");
 		break;
 		}
 
-		printf("Got data : %s & length : %d\n" ,s,strlen(s));
+		printf("Got data : %s & length : %d\n" ,word,strlen(word));
+		k++;
+		
+
+		//adding values to 2D array
+		for(i=0;i<strlen(word);i++){
+			for(j=0;j<k;j++){
+			matrix[i][j] = (char) word;
+			scanf("%s",&matrix[i][j]);
+			printf("%s", matrix[i][j]);
+			}
+		}
+			printf("%s", matrix[i][j]);
+	}
+
+		while(1){
+		//gets(word);
+		//word = getchar(); 		
+		printf("\nLength of input : %d\n", strlen(word));
+ 		/*if(s[0] == NULL){
+			printf("Null test 1");
+			break;
+		}*/
+		if(strlen(word) == 0){
+			printf("Null test 2");
+		break;
+		}
+
+		printf("Got data : %s & length : %d\n" ,word,strlen(word));
 		k++;
 	}
 
