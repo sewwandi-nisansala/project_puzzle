ea85c4fcd971ff7848ff206110f60ad6011f2b6b
sewwanis <sewwanis@gmail.com>
2018-07-03 07:21:13 -0700

    added 2 linked lists

--- project.c
+++ project.c
@@ -2,70 +2,107 @@
 #include <stdlib.h>
 #include <string.h>
 
-/*struct node{
-	int length;
-	struct Node *next;
-};
-*/
+typedef struct node1
+{
+    char list1[100];
+    struct node1* next;
+} node1;
+ 
 
+typedef struct node2
+{
+    char list2[100];
+    struct node2* next;
+} node2;
 
+typedef void (*callback)(node1* node1);
+typedef void (*callback)(node1* node2);
+ 
 //this function is to print the matrix
-void print_matrix(char matrix,int n){
+//void print_matrix(char matrix,int n){
 
-}
+
 int main(){
 
-	char word[100];
+	char words[100];
+	char inputs[100];
 	int k = 1;
-	char matrix[i][j];
-	int i,j;
-
+	//char matrix[i][j];
+	
 	while(1){
-		//gets(word);
-		word = getchar();
- 		printf("\nLength of input : %d\n", strlen(word));
- 		/*if(word[0] == NULL){
-			printf("Null test 1");
-			break;
-		}*/
-		if(strlen(word) == 0){
+		gets(words);
+		//words = getchar();
+ 		printf("\nLength of input : %d\n", strlen(words));
+ 		
+		if(strlen(words) == 0){
 			printf("Null test 2");
 		break;
 		}
 
-		printf("Got data : %s & length : %d\n" ,word,strlen(word));
+		printf("Got data : %s & length : %d\n" ,words,strlen(words));
 		k++;
 		
 
-		//adding values to 2D array
-		for(i=0;i<strlen(word);i++){
-			for(j=0;j<k;j++){
-			matrix[i][j] = (char) word;
-			scanf("%s",&matrix[i][j]);
-			printf("%s", matrix[i][j]);
-			}
+//adding data(word) to a linked list
+		/*
+    		create a new node
+   		 initialize the data and next field
+ 
+   		 return the newly created node
+		*/
+		node1* create(char list1,node1* next){
+   		node1* new_node1 = (node1*)malloc(sizeof(node1));
+    		if(new_node1 == NULL){
+   			printf("Error creating a new node1.\n");
+      			exit(0);
+   		 }
+    		//new_node1->list1 = words;
+		strcpy(new_node1->list1,words);
+    		new_node1->next = next;
+ 
+    		return new_node1;
 		}
-			printf("%s", matrix[i][j]);
 	}
 
 		while(1){
-		//gets(word);
-		//word = getchar(); 		
-		printf("\nLength of input : %d\n", strlen(word));
- 		/*if(s[0] == NULL){
-			printf("Null test 1");
-			break;
-		}*/
-		if(strlen(word) == 0){
+		
+		gets(inputs);
+		//inputs = getchar(); 		
+		printf("\nLength of input : %d\n", strlen(inputs));
+ 		
+		if(strlen(inputs) == 0){
 			printf("Null test 2");
 		break;
 		}
 
-		printf("Got data : %s & length : %d\n" ,word,strlen(word));
+//again add data to another linked list
+/*
+    		create a new node
+   		 initialize the data and next field
+ 
+   		 return the newly created node
+		*/
+		node2* create(char list2,node2* next){
+   		node2* new_node2 = (node2*)malloc(sizeof(node2));
+    		if(new_node2 == NULL){
+   			printf("Error creating a new node2.\n");
+      			exit(0);
+   		 }
+    		//new_node2->list2 = inputs;
+		strcpy(new_node2->list2,inputs);
+    		new_node2->next = next;
+ 
+    		return new_node2;
+		}
+
+
+		printf("Got data : %s & length : %d\n" ,inputs,strlen(inputs));
 		k++;
 	}
 
 	
+//make 2D array obove linked lists
+
 	printf("\n number of Node Reads : %d\n",k);
 return 0;
 }
